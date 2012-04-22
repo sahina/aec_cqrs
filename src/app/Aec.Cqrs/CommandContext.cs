bf7970002cf960569a81fafe83c13e81da6bc20b
Altug Sahin <altug@aecoffice.com>
2012-04-22 17:51:48 -0400

    updated cmd context

---
+++ src/app/Aec.Cqrs/CommandContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aec.Cqrs
+{
+    [Serializable]
+    public class CommandContext
+    {
+        public Guid CommandID { get; set; }
+        public string Issuer { get; set; }
+        public string IssuerHost { get; set; }
+        public DateTime IssueDateUtc { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+
+        public CommandContext()
+        {
+            Headers = new Dictionary<string, string>();
+        }
+    }
+}
