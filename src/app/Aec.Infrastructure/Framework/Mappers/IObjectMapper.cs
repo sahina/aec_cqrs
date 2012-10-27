2185722ed5fb5623464dee18107dc9ad6608c81b
Altug Sahin <altug@aecoffice.com>
2012-10-27 09:09:44 -0400

    added mapper interface

---
+++ src/app/Aec.Infrastructure/Framework/Mappers/IObjectMapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Aec.Infrastructure.Framework.Mappers
+{
+    public interface IObjectMapper
+    {
+        List<TResult> MapTo<TResult>(IEnumerable source);
+        TResult MapTo<TResult>(object source);
+        TResult MapPropertiesToInstance<TResult>(object source, TResult value);
+        TResult DynamicMapTo<TResult>(object source);
+    }
+}
