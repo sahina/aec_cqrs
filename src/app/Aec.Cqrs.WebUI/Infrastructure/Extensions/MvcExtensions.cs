a066d8edbe0a490773328505e97c5767e85f9324
Altug Sahin <altug@aecoffice.com>
2012-10-27 09:05:04 -0400

    added extensions to web project

---
+++ src/app/Aec.Cqrs.WebUI/Infrastructure/Extensions/MvcExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Aec.Cqrs.WebUI.Infrastructure.Extensions
+{
+    public static class MvcExtensions
+    {
+        public static string Errors(this ModelStateDictionary state)
+        {
+            var errors = new List<string>();
+
+            foreach (var error in state.Values)
+            {
+                errors.AddRange(error.Errors.Select(
+                    err => string.IsNullOrEmpty(err.ErrorMessage) ? err.Exception.Message : err.ErrorMessage));
+            }
+
+            return string.Join(" ", errors).Trim();
+        }
+    }
+}
